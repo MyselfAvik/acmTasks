e87e24175b969a8cdc18adbd10a4671ae6ee0f52
Avik Dangar <123013809+MyselfAvik@users.noreply.github.com>
2024-03-20 19:38:27 +0530

    q4 done

--- q4.java
+++ q4.java
@@ -2,19 +2,29 @@ import java.util.*;
 public class q4{
     public static void main(String args[]){
         Scanner sc =new Scanner(System.in);
-        int count=0;
+        
+        int x=sc.nextInt();
+        for(int i=0;i<x;i++){
+        
         int n=sc.nextInt();
         String s=sc.next();
-        for(int i=0;i<n-2;i++){
-            if(s.charAt(i)=='p' && s.charAt(i+1)=='i' && s.charAt(i+2)=='e'){
-                count++;
-                i+=2;
-            }
-            else if(s.charAt(i)=='m' && s.charAt(i+1)=='a' && s.charAt(i+2)=='p'){
-                count++;
-                i+=2;
-            }
+        int answer=ans(s);
+        System.out.println(answer);
         }
-        System.out.println(count);
     }
+        public static int ans(String s){
+            int count=0;
+            int n=s.length();
+            for(int i=0;i<n-2;i++){
+                if(s.charAt(i)=='p' && s.charAt(i+1)=='i' && s.charAt(i+2)=='e'){
+                    count++;
+                    i+=2;
+                }
+                else if(s.charAt(i)=='m' && s.charAt(i+1)=='a' && s.charAt(i+2)=='p'){
+                    count++;
+                    i+=2;
+                }
+            } 
+            return count; 
+        }
 }
