3845ec64d36201223a0ce9d1ba29ce8dcf88dc38
Avik Dangar <123013809+MyselfAvik@users.noreply.github.com>
2024-03-20 19:39:23 +0530

    Create tempCodeRunnerFile.java

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class q4{
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        int count=0;
+        int x=sc.nextInt();
+        for(int i=0;i<x;i++){
+        int n=sc.nextInt();
+        String s=sc.next();
+        for(i=0;i<n-2;i++){
